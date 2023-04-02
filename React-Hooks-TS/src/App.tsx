31931a37c6d8ce0497fdb0bcc9dfa6b3ac3c3cb1
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-04-02 23:08:19 +0200

    React Hooks + TS

---
+++ React-Hooks-TS/src/App.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useEffect, useState, useMemo, MouseEvent, KeyboardEvent, useRef } from "react"
+
+interface User {
+  id: number,
+  username: string,
+}
+
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n
+
+  return fib(n - 1) + fib(n - 2)
+}
+
+const myNum: number = 11
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+  const [users, setUsers] = useState<User[] | null>(null)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
+
+  useEffect(() => {
+    console.log('mounting');
+    console.log('Users:', users);
+
+    return () => console.log('unmointing');
+
+  }, [users])
+
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => setCount(prev => prev + 1), [])
+
+  const result = useMemo<number>(() => fib(myNum), [myNum])
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text" />
+    </div>
+  )
+}
+
+export default App
